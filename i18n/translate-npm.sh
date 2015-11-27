6f538fe397c12f8c799b431a542b55bde586b2d5
Michael J. Bennett <michael.bennett@rangle.io>
2015-11-27 16:57:32 -0500

    (WIP) bootstraps an example project for i18n

---
+++ i18n/translate-npm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+npm install --save gulp-angular-translate angular-translate
+
+npm install --save angular-message-format messageformat \
+angular-translate-interpolation-messageformat
---
+++ i18n/workshop/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('workshop', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'toastr']);
+
+})();
